1db898c4d2561a9336f07001741f333eaadf50b8
Carlos <cchavezmx@outlook.com>
2021-08-26 17:37:05 -0500

    primera idea

---
+++ src/components/InputSalidas.js
@@ -0,0 +1,12 @@
+const InputSalidas = () => {
+  return (
+    <div className="input__Container">
+        <label>
+          Ingrese el codigo aqui
+          <input placeholder="Ingrese el codigo de la tarjeta" />
+        </label>
+    </div>
+  )
+}
+
+export default InputSalidas
---
+++ src/view/Salidas/Salidas.js
@@ -0,0 +1,11 @@
+import InputSalidas from "components/InputSalidas"
+
+const Salidas = () => {
+  return (
+    <div>
+      <InputSalidas />
+    </div>
+  )
+}
+
+export default Salidas
